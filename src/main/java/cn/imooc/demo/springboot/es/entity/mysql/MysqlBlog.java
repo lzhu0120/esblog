2b243de3efbfb3d68667ac363b935ed07da61ed4
zhuli <l84zhu@uwaterloo.ca>
2020-12-03 14:51:14 -0800

    second commit

--- src/main/java/cn/imooc/demo/springboot/es/entity/mysql/MysqlBlog.java
+++ src/main/java/cn/imooc/demo/springboot/es/entity/mysql/MysqlBlog.java
@@ -7,18 +7,6 @@ import javax.persistence.*;
 
 //maven中的lombok插件 针对实体经常需要大量的setter、getter、 hashCode、 equals、 toString方法，通过此工具可以免去相关处理
 
-/**
- * CREATE TABLE 't_blog' (
- * 'id' int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
- * 'title' varchar(60) DEFAULT NULL COMMENT '博客标题',
- * 'author' varchar(60) DEFAULT NULL COMMENT '博客作者',
- * 'content' mediumtext COMMENT '博客内容',
- * 'create_time' datetime DEFAULT NULL COMMENT '创建时间',
- * 'update_time' datetime DEFAULT NULL COMMENT '更新时间',
- * PRIMARY KEY ('id')
- * ) ENGINE = InnoDB AUTO_INCREMENT=12, DEFAULT CHARSET=utf8mb4;
- *
- * **/
 
 /**
  * @author lz
